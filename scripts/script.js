d0ee55f8b0d051c31163189f751c31f76c730558
ItsMeIkee <ike4900@gmail.com>
2024-05-06 11:46:10 -0400

    Added more puzzles
    The extra buttons redirect to different puzzle pages. They all work the same with a few minor changes between them. The title and the actual puzzle.

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,19 @@
 function startGame() {
   var difficulty = document.getElementById('difficulty').value; // Get the selected difficulty from the dropdown
   // Redirect to the puzzle page with the selected difficulty as a query parameter
-  window.location.href = `puzzle.html?difficulty=${difficulty}`;
+
+
+
+  document.getElementById('aniB').onclick = function () {
+    window.location.href = `puzzle.html?difficulty=${difficulty}`;
+  };
+  document.getElementById('gdB').onclick = function () {
+    window.location.href = `puzzleB.html?difficulty=${difficulty}`;
+  }
+
+  document.getElementById('basB').onclick = function () {
+    window.location.href = `puzzleC.html?difficulty=${difficulty}`;
+  }
 }
 
 
